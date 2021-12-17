a6df6fb3af7e6cf9e10160ba5209e659375b2279
Andrewerr <p01ar@p01ar.net>
2021-12-17 16:36:47 +0200

    [vendor][prebuilts]: Removed some unneeded parts of microg

--- Android.mk
+++ Android.mk
@@ -1,3 +1,3 @@
 CODENAME := "opressor"
-BUILD_NUMBER_FROM_FILE := "PolarMod-1.1-alpha2-${signature}"
+BUILD_NUMBER_FROM_FILE := "PolarMod-1.1-alpha3-${signature}"
 
--- prebuilts/MozillaNlpBackend/Android.mk
+++ prebuilts/MozillaNlpBackend/Android.mk
@@ -1,4 +1,14 @@
 LOCAL_PATH := $(call my-dir)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := default-permissions-MozillaNlpBackend
+LOCAL_SRC_FILES := default-permissions-MozillaNlpBackend.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/default-permissions
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := MozillaNlpBackend
@@ -6,4 +16,5 @@ LOCAL_SRC_FILES := org.microg.nlp.backend.ichnaea.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := platform
+LOCAL_REQUIRED_MODULES := default-permissions-MozillaNlpBackend
 include $(BUILD_PREBUILT)

f0d69ba36a0d303fb45268f4e45589f9c482e0e4
Andrewerr <p01ar@p01ar.net>
2022-01-29 21:39:44 +0200

    [vendor][systemui]: added G-Visual Mod

--- config/config.mk
+++ config/config.mk
@@ -12,11 +12,15 @@ PRODUCT_PACKAGES += \
     Updater \
 		OminJaws \
     FaceUnlockService
-    
+
+# FaceUnlock and blur on non-go systems
+ifneq ($(PRODUCT_TYPE), go)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
    ro.surface_flinger.supports_background_blur=1 \
    ro.face_unlock_service.enabled=true 
+endif
 
+# MicroG
 ifeq ($(TARGET_USES_MICROG), 1)
 PRODUCT_PACKAGES += \
     additional_repos \
@@ -30,3 +34,12 @@ PRODUCT_PACKAGES += \
     microg-a5k.xml 
 endif
 
+
+# G-Visual Mod
+PRODUCT_PACKAGES += \
+    GVM-SBH-L \
+    GVM-SBH-M \
+    GVM-SBH-XL \
+    GVM-URM-M \
+    GVM-URM-L \
+    GVM-URM-R
---
+++ overlays/README.md
@@ -0,0 +1,2 @@
+# Overlays
+Overlays stored here are intended to be used with `Droidbuild`. Do not put any other overlays here.

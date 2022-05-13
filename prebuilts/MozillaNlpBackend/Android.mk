090cb8ebcc4d58e8a1c9016588255f1b1589d5ee
Andrewerr <p01ar@p01ar.net>
2022-05-13 12:20:30 +0300

    [prebuilts]: MozillaNlpBackend is now flagged as PRESIGNED

--- prebuilts/MozillaNlpBackend/Android.mk
+++ prebuilts/MozillaNlpBackend/Android.mk
@@ -15,6 +15,6 @@ LOCAL_MODULE := MozillaNlpBackend
 LOCAL_SRC_FILES := org.microg.nlp.backend.ichnaea.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_REQUIRED_MODULES := default-permissions-MozillaNlpBackend
 include $(BUILD_PREBUILT)

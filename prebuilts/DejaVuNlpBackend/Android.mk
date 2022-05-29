f727bb1b2d7a359f710e3762d4745c2e09c8edcd
Andrewerr <p01ar@p01ar.net>
2022-05-29 19:36:24 +0300

    [vendor] [prebuilts]: Added DejaVu NLP backend, fixed default permissions for MozillaNlpBackend

--- prebuilts/DejaVuNlpBackend/Android.mk
+++ prebuilts/DejaVuNlpBackend/Android.mk
@@ -3,19 +3,19 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := default-permissions-MozillaNlpBackend.xml
-LOCAL_SRC_FILES := default-permissions-MozillaNlpBackend.xml
+LOCAL_MODULE := default-permissions-DejaVuNlpBackend.xml
+LOCAL_SRC_FILES := default-permissions-DejaVuNlpBackend.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/default-permissions
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := MozillaNlpBackend
-LOCAL_SRC_FILES := org.microg.nlp.backend.ichnaea.apk
+LOCAL_MODULE := DejaVuNlpBackend
+LOCAL_SRC_FILES := DejaVuNlpBackend.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_PRIVELEGED_MODULE := true
-LOCAL_REQUIRED_MODULES := default-permissions-MozillaNlpBackend.xml
+LOCAL_REQUIRED_MODULES := default-permissions-DejaVuNlpBackend.xml
 include $(BUILD_PREBUILT)

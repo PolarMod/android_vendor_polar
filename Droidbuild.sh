b42d8136746a246b0b3082c5bb57740bef02a6f0
Andrewerr <toddot@protonmail.com>
2022-08-09 14:53:05 +0300

    [FIX] [overlays]: disable NetworkStack overlay patching

--- Droidbuild.sh
+++ Droidbuild.sh
@@ -7,10 +7,10 @@ droidbuild_module(){
   if ndef VENDOR_POLAR_LOADED
   then  
     require_command patch
-    info "Patching NetworkStack overlay"
-    exec rm -f $BASEDIR/vendor/lineage/overlay/common/packages/modules/NetworkStack/res/values/config.xml
-    exec cp $LOCAL_PATH/overlays/networkstack_config.xml $BASEDIR/vendor/lineage/overlay/common/packages/modules/NetworkStack/res/values/config.xml
-    success "Patched NetworkStack overlay"
+    #info "Patching NetworkStack overlay"
+    #exec rm -f $BASEDIR/vendor/lineage/overlay/common/packages/modules/NetworkStack/res/values/config.xml
+    #exec cp $LOCAL_PATH/overlays/networkstack_config.xml $BASEDIR/vendor/lineage/overlay/common/packages/modules/NetworkStack/res/values/config.xml
+    #success "Patched NetworkStack overlay"
     info "Adding PolarMod vendor to Lineage build system"
     exec patch $BASEDIR/vendor/lineage/config/common.mk $LOCAL_PATH/patches/common.mk.patch
     success "Patched Lineage vendor"

7a7b98d65281e1b362f545c3dec675d91fe1aaba
Andrewerr <toddot@protonmail.com>
2022-10-10 22:47:11 +0300

    Revert "[vendor] [gmscompat]: ship Google Maps jar from microG with GmsCompat"
    This reverts commit 5ad60c84990d9020669775076ec8e51866ee618b.

--- config/config.mk
+++ config/config.mk
@@ -40,10 +40,7 @@ endif
 
 # GmsCompat
 ifeq ($(TARGET_SUPPORTS_GMSCOMPAT), 1)
-PRODUCT_PACKAGES += \
-   GmsCompat \
-   com.google.android.maps.xml \
-   com.google.android.maps.jar
+PRODUCT_PACKAGES += GmsCompat
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
    ro.system.gms_compat=true \
    ro.build.stock_fingerprint=$(PRODUCT_STOCK_FINGERPRINT)

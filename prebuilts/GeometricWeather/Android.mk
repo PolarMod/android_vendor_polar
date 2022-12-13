8519fc945dac6ba47c325edc2d41427e897a9643
Andrewerr <toddot@protonmail.com>
2022-12-13 16:31:08 +0100

    [FIX] [prebuilts]: add missing libraries for GeometricWeather

--- prebuilts/GeometricWeather/Android.mk
+++ prebuilts/GeometricWeather/Android.mk
@@ -6,4 +6,7 @@ LOCAL_SRC_FILES := GeometricWeather.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_OPTIONAL_USES_LIBRARIES += \
+		androidx.window.extensions \
+		androidx.window.sidecar
 include $(BUILD_PREBUILT)
